12af8a376740a0d4e7e317748814264282d578a9
yoshow <ruanyu@live.com>
2014-11-20 09:35:29 +0800

    更新示例

---
+++ projects/x-client-api/examples/index.html.js
@@ -0,0 +1,147 @@
+﻿$(document).ready(function()
+{
+    // 设置菜单数据
+    var menudata = [
+
+    { "id": "2", "text": "常用函数", "value": "", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": true, "complete": true, "classes": null, "data": null, "ChildNodes": [
+        { "id": "2.1", "text": "工具函数", "value": "x.util.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "2.2", "text": "Date 函数", "value": "x.date.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "2.3", "text": "Cookies 函数", "value": "x.cookies.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "2.4", "text": "Css 函数", "value": "x.css.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "2.5", "text": "Page 函数", "value": "x.page.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "2.1", "text": "Drag 函数", "value": "x.drag.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": []}]
+    },
+    
+     { "id": "1", "text": "常用组件", "value": "", "showcheck": false, "isexpand": true, "checkstate": 0, "hasChildren": true, "complete": true, "classes": null, "data": null, "ChildNodes": [
+        { "id": "1.4", "text": "表单", "value": "x.form.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.1", "text": "日历", "value": "x.calendar.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.2", "text": "组合框", "value": "x.combobox.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.9", "text": "普通菜单", "value": "x.menu.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.3", "text": "上下文菜单", "value": "x.contextMenu.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.3", "text": "工具提示", "value": "x.tooltip.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.5", "text": "树形", "value": "x.tree.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.6", "text": "网格", "value": "x.grid.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.7", "text": "页签", "value": "x.tabs.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "1.8", "text": "甘特图", "value": "x.gantt.html", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": []}]
+    },
+    { "id": "3", "text": "对话向导", "value": "", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": true, "complete": true, "classes": null, "data": null, "ChildNodes": [
+        { "id": "3.1", "text": "我的项目", "value": "http://xgcalendar.cloudapp.net/", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "3.2", "text": "项目管理", "value": "/Home/Index2", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "3.3", "text": "项目周报", "value": "/MeetRoomUseage/MeetRoomUseageList", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "3.4", "text": "人员周报", "value": "/MeetRoomUseage/MeetRoomUseageList", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": []}]
+    },
+    { "id": "4", "text": "系统管理", "value": "", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": true, "complete": true, "classes": null, "data": null, "ChildNodes": [
+        { "id": "4.1", "text": "人员管理", "value": "/System/UserList", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "4.2", "text": "参数管理", "value": "/SysManage/PrintTemplateList", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": [] },
+        { "id": "4.3", "text": "关于我们", "value": "/Home/About", "showcheck": false, "isexpand": false, "checkstate": 0, "hasChildren": false, "complete": true, "classes": null, "data": null, "ChildNodes": []}]
+    }];
+
+    var ieVersion = getIEVersion();
+
+    // 获得IE版本
+    function getIEVersion()
+    {
+        var userAgent = window.navigator.userAgent.toLowerCase();
+
+        x.browser.msie9 = x.browser.ie && /msie 9\.0/i.test(userAgent);
+
+        x.browser.msie8 = x.browser.ie && /msie 8\.0/i.test(userAgent);
+
+        x.browser.msie7 = x.browser.ie && /msie 7\.0/i.test(userAgent);
+
+        x.browser.msie6 = !x.browser.ie9 && !x.browser.msie8 && !x.browser.msie7 && x.browser.msie && /msie 6\.0/i.test(userAgent);
+
+        var version;
+
+        if (x.browser.msie9)
+        {
+            version = 9;
+        }
+        else if (x.browser.msie8)
+        {
+            version = 8;
+        }
+        else if (x.browser.msie7)
+        {
+            version = 7;
+        }
+        else if (x.browser.msie6)
+        {
+            version = 6;
+        }
+        else
+        {
+            version = -1;
+        }
+
+        return version;
+    }
+
+    if (ieVersion > 0)
+    {
+        $(document.body).addClass("ie ie" + ieVersion);
+    }
+
+    // 设置窗口大小
+    var mainh = document.documentElement.clientHeight;
+    var mainw = document.documentElement.clientWidth;
+
+    var panelheader = $("#panel-header").outerHeight();
+    var header = 38;
+    var bottom = 30;
+
+    // 设置窗口大小
+    $("#treeViewPanel").treeview({ data: menudata, onnodeclick: navi });
+
+    var tabo = {};
+
+    tabo.items = [{ id: "home", text: "主页", classes: "icon_home", isactive: true, content: "<div style='padding:10px;'>欢迎光临!</div>"}];
+    tabo.width = mainw - 180;
+    tabo.height = mainh - header - bottom;
+
+    $("#tab-container").tabpanel(tabo);
+
+    function navi(item)
+    {
+        if (item.value)
+        {
+            $("#tab-container").opentabitem({ id: idreplace(item.id), text: item.text, url: item.value, isactive: true, closeable: true }, true);
+        }
+        else
+        {
+            item.expand();
+        }
+    }
+
+    function idreplace(id)
+    {
+        return id.replace(/[^a-zA-Z\d_]/ig, "_")
+    }
+
+    // 设置面板大小
+    setpanelsize(ieVersion);
+
+    function setpanelsize(ieVersion)
+    {
+        mainh = document.documentElement.clientHeight;
+        mainw = document.documentElement.clientWidth;
+
+        var height = mainh - header - panelheader - bottom + 3;
+        var width = mainw - 180;
+
+        if (ieVersion > 0)
+        {
+            height = height - 1;
+        }
+
+        $("#panel-wrap").height(height);
+        $("#tab-container").width(width);
+    }
+
+    // 调整大小
+    $(window).resize(function window_resize()
+    {
+        setpanelsize();
+        $("#tab-container").resizetabpanel(mainw - 180, mainh - header - bottom);
+    });
+});
---
+++ projects/x-client-api/examples/x.date.html.js
@@ -0,0 +1,102 @@
+﻿// 配置请求路径
+require.config({
+    paths: {
+        'jquery': '/resources/javascript/jquery-2.0.3.min',
+        'x': '/resources/javascript/core/x',
+        'x.debug': '/resources/javascript/core/x.debug',
+        'x.date': '/resources/javascript/core/x.date'
+    }
+});
+
+require([
+    'jquery',
+    'x',
+    'x.debug',
+    'x.date'
+],
+function($, x)
+{
+    $(document).ready(function()
+    {
+        var outString = '';
+
+        outString += '<ul>';
+
+        outString += '<li>测试 x.date 模块开始.</li>';
+
+        outString += '<li>初始化</li>';
+        outString += '<li>运行 x.date.create(new Date()). 结果:[' + x.date.create(new Date()) + '].</li>';
+        outString += '<li>运行 x.date.create([2000,1,1]). 结果:[' + x.date.create([2000, 1, 1]) + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-01\'). 结果:[' + x.date.create('2000-01-01') + '].</li>';
+        outString += '<li>运行 x.date.create(\'2013/11/7 15:14:40\'). 结果:[' + x.date.create('2013/11/7 15:14:40') + '].</li>';
+
+        outString += '<li>运算</li>';
+        outString += '<li>运行 x.date.add(\'2013-01-18 10:53:54\',\'minute\',5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2013-01-18 10:53:54', 'minute', 5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2013-01-18 10:53:54\',\'minute\',-5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2013-01-18 10:53:54', 'minute', -5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'year\',5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'year', 5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-02-29\',\'quarter\',3).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-02-29', 'quarter', 3).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-02-29\',\'year\',3).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-02-29', 'year', 3).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-02-29\',\'year\',4).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-02-29', 'year', 4).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'quarter\',2).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'quarter', 2).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'month\',5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'month', 5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-01-31\',\'month\',1).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-01-31', 'month', 1).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-02-28\',\'month\',1).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-02-28', 'month', 1).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-02-28\',\'month\',11).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-02-28', 'month', 11).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2012-02-29\',\'month\',12).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2012-02-29', 'month', 12).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-03-31\',\'month\',1).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-03-31', 'month', 1).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-09-30\',\'month\',1).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-09-30', 'month', 1).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-10-31\',\'month\',1).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-10-31', 'month', 1).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2014-12-31\',\'month\',1).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2014-12-31', 'month', 1).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'day\',60).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'day', 60).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'day\',-60).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'day', -60).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'hour\',60).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'hour', 60).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'hour\',-60).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'hour', -60).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2000-01-01\',\'minute\',5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2000-01-01', 'minute', 5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2013-01-18 10:53:54\',\'minute\',5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2013-01-18 10:53:54', 'minute', 5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+        outString += '<li>运行 x.date.add(\'2013-01-18 10:53:54\',\'minute\',-5).toString(\'yyyy-MM-dd HH:mm:ss\'). 结果:[' + x.date.add('2013-01-18 10:53:54', 'minute', -5).toString('yyyy-MM-dd HH:mm:ss') + '].</li>';
+
+        outString += '<li>一年中的第几周</li>';
+        outString += '<li>运行 x.date.create(new Date()).getWeekOfYear(). 结果:[' + x.date.create(new Date()).getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-01\').getDayOfYear(). 结果:[' + x.date.create('2000-01-01').getDayOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-01\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-01').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-02\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-02').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-03\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-03').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-04\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-04').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-05\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-05').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-06\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-06').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-01-07\').getWeekOfYear(). 结果:[' + x.date.create('2000-01-07').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-02-01\').getDayOfYear(). 结果:[' + x.date.create('2000-02-01').getDayOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-02-01\').getWeekOfYear(). 结果:[' + x.date.create('2000-02-01').getWeekOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-12-31\').getDayOfYear(). 结果:[' + x.date.create('2000-12-31').getDayOfYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-12-31\').getWeekOfYear(). 结果:[' + x.date.create('2000-12-31').getWeekOfYear() + '].</li>';
+
+        outString += '<li>本月最大的天数</li>';
+        outString += '<li>运行 x.date.create(new Date()).getMaxDayOfMonth(). 结果:[' + x.date.create(new Date()).getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'1900-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('1900-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('2000-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2002-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('2002-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2004-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('2004-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2006-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('2006-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2008-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('2008-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2010-02-01\').getMaxDayOfMonth(). 结果:[' + x.date.create('2010-02-01').getMaxDayOfMonth() + '].</li>';
+        outString += '<li>闰年判断</li>';
+        outString += '<li>运行 x.date.create(new Date()).isLeapYear(). 结果:[' + x.date.create(new Date()).isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'1900\').isLeapYear(). 结果:[' + x.date.create('1900').isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2000\').isLeapYear(). 结果:[' + x.date.create('2000').isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2002\').isLeapYear(). 结果:[' + x.date.create('2002').isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2004\').isLeapYear(). 结果:[' + x.date.create('2004').isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2006\').isLeapYear(). 结果:[' + x.date.create('2006').isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2008\').isLeapYear(). 结果:[' + x.date.create('2008').isLeapYear() + '].</li>';
+        outString += '<li>运行 x.date.create(\'2010\').isLeapYear(). 结果:[' + x.date.create('2010').isLeapYear() + '].</li>';
+        outString += '<li>格式化</li>';
+        outString += '<li>运行 x.date.create(new Date()).toString(\'yyyy\MM\dd\'). 结果:[' + x.date.create(new Date()).toString('yyyy\MM\dd') + '].</li>';
+        outString += '<li>运行 x.date.create(new Date()).toString(\'yy\MM\dd\'). 结果:[' + x.date.create(new Date()).toString('yy\MM\dd') + '].</li>';
+        outString += '<li>运行 x.date.create(new Date()).toString(\'yy-MM-dd HH:mm:ss\'). 结果:[' + x.date.create(new Date()).toString('yy-MM-dd HH:mm:ss') + '].</li>';
+
+        outString += '<li>测试 x.date 模块结束.</li>';
+
+        outString += '</ul>';
+
+        $('div#windowContainer').html(outString);
+    });
+});
